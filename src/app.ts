b55da492189376797f8c491a104a58529cbbe286
JhustynC <christopherarias2002@gmail.com>
2024-09-18 23:58:58 -0500

    First Web Server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log('REST-Web');
+import http from "http";
+
+const server = http.createServer((req, res) => { 
+  
+
+  console.log(req.url);
+
+
+  // res.writeHead(200, { "Content-Type": "application/json" });
+  // res.write(JSON.stringify({ message: "Hello, World!" }));
+  res.write("Hello, World!");
+  res.end();
+});
+
+server.listen(8080, () => {
+  console.log("Server is running on port 8080");
+});
