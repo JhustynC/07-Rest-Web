383b35e77e4a81308b32589ca6bdf5801c12972a
JhustynC <christopherarias2002@gmail.com>
2024-09-24 23:37:41 -0500

    Use Cases Files

--- src/domain/use-cases/todos.ts
+++ src/domain/use-cases/todos.ts
@@ -7,3 +7,6 @@ export class CreateTodoUseCase implements ICreateTodoUseCase {
     throw new Error("Method not implemented.");
   }
 }
+
+//? Se pueden crear los diferentes casos de un solo modulo en un archivo
+//? O es necesario que este casa caso de uso en un solo archivo 
