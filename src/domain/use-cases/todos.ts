a2e8c93b309e56c0819e5d2cb2603dd27f5eb810
JhustynC <christopherarias2002@gmail.com>
2024-09-26 23:56:55 -0500

    Create a interface for todos use cases

--- src/domain/use-cases/todos.ts
+++ src/domain/use-cases/todos.ts
@@ -1,9 +1,15 @@
-interface ICreateTodoUseCase {
-  createTodo(title: string, description: string): void;
+interface ITodoUseCase {
+  execute(title: string, description: string): void;
 }
 
-export class CreateTodoUseCase implements ICreateTodoUseCase {
-  createTodo(title: string, description: string): void {
+export class CreateTodoUseCase implements ITodoUseCase {
+  execute(title: string, description: string): void {
+    throw new Error("Method not implemented.");
+  }
+}
+
+export class DeleteTodosUseCase implements ITodoUseCase {
+  execute(title: string, description: string): void {
     throw new Error("Method not implemented.");
   }
 }
