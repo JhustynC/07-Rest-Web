6131692cd7c40904dcdcb00ac088f46c5fb28bb7
JhustynC <christopherarias2002@gmail.com>
2024-10-06 23:05:48 -0500

    Delete todo datasource implementation

--- src/infrastructure/datasources/todo.datasource.ts
+++ src/infrastructure/datasources/todo.datasource.ts
@@ -1,14 +1,21 @@
 import { prisma } from "../../data/postgres";
-import { AbsTodoDatasource, CreateTodoDto, TodoEntity, UpdateTodoDto } from "../../domain";
+import {
+  AbsTodoDatasource,
+  CreateTodoDto,
+  TodoEntity,
+  UpdateTodoDto,
+} from "../../domain";
 
 export class TodoDatasourceImp implements AbsTodoDatasource {
   create(createTodoDto: CreateTodoDto): Promise<TodoEntity> {
     throw new Error("Method not implemented.");
   }
+
   async getAll(): Promise<TodoEntity[]> {
     const todos = await prisma.todo.findMany();
     return todos.map(TodoEntity.fromObject);
   }
+
   async getById(id: number): Promise<TodoEntity | undefined> {
     const todoObject = await prisma.todo.findUnique({
       where: {
@@ -18,8 +25,9 @@ export class TodoDatasourceImp implements AbsTodoDatasource {
 
     return todoObject ? TodoEntity.fromObject(todoObject) : undefined;
   }
+
   async update(updateTodoDto: UpdateTodoDto): Promise<TodoEntity | undefined> {
-    const { title, id } = updateTodoDto
+    const { title, id } = updateTodoDto;
     const todo = await prisma.todo.update({
       where: {
         id,
@@ -28,11 +36,15 @@ export class TodoDatasourceImp implements AbsTodoDatasource {
         title,
       },
     });
-    return todo? TodoEntity.fromObject(todo) : undefined;
+    return todo ? TodoEntity.fromObject(todo) : undefined;
   }
 
-  delete(id: number): Promise<void> {
-    console.log("implementation");
-    return Promise.resolve();
+  async delete(id: number): Promise<void> {
+    const todo = await prisma.todo.delete({
+      where: {
+        id,
+      },
+    });
+    return todo ? Promise.resolve() : undefined;
   }
 }
