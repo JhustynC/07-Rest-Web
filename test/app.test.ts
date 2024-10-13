e89d714a8cc9aeb723de85bd39d20244bb54a7d6
JhustynC <christopherarias2002@gmail.com>
2024-10-12 23:23:47 -0500

    App.ts test imp

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,5 +1,20 @@
-describe("Testing App.ts", () => {
-  test("", () => {
-    expect(true).toBe(true);
+import { AnyTypeAnnotation } from "./../node_modules/@babel/types/lib/index-legacy.d";
+import { Router } from "express";
+import { envs } from "../src/config/plugins/evns.plugin,";
+import { Server } from "../src/presentation/server";
+
+jest.mock("../src/presentation/server");
+
+describe("Should call server with arguments ans start", () => {
+  test("", async () => {
+    await import("../src/app");
+    expect(Server).toHaveBeenCalledTimes(1);
+    expect(Server).toHaveBeenCalledWith({
+      port: envs.PORT,
+      publicPath: envs.PUBLIC_PATH,
+      routes: expect.any(Function),
+    });
+
+    expect(Server.prototype.start).toHaveBeenCalledWith();
   });
 });
